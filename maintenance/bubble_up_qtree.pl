29815de3d87c8ce51ca5c044deaaf83fb64993b6
Daniel Schwen <toolserver@schwen.de>
2011-03-24 22:08:27 +0000

    loop over zooms

--- maintenance/bubble_up_qtree.pl
+++ maintenance/bubble_up_qtree.pl
@@ -51,41 +51,49 @@ my $db = DBI->connect(
 
 print "Connected.\n";
   
-$zoom = $maxzoom-1;
-
-$query = "DELETE l.* FROM wma_label l, wma_tile t WHERE l.lang_id='$langid' AND l.rev='$rev' AND t.rev='$rev' AND t.z='$zoom';";
-$sth = $db->prepare( $query );
-$rows = $sth->execute;
-print "Delete $rows labels in zoom $zoom from previous run.\n";
-
-$start = time();
-$query = "SELECT /* SLOW_OK */  FLOOR(x/2) as x2, FLOOR(y/2) as y2, t.id, l.weight, page_id FROM wma_label l, wma_tile t WHERE t.id = l.tile_id AND t.z='".($zoom+1)."' AND l.rev='$rev' AND t.rev='$rev' AND lang_id='$langid' ORDER BY x2,y2,l.weight LIMIT 20;"; 
-print "$query\n";
-$sth = $db->prepare( $query );
-$sth->execute;
-
-$lasttid = -1;
-while( @row = $sth->fetchrow() ) 
+for( $zoom = $maxzoom-1; $zoom >= 0; $zoom-- ) 
 {
-  ( $x, $y, $tid, $weight, $pageid ) = @row[0..4];
-print "$x, $y, $tid, $weight, $pageid \n";
-  # did we already insert a tile?
-  $query = "SELECT id FROM wma_tile WHERE z='$zoom' AND x='$x' AND y='$y' AND rev='$rev';";
-  $sth2 = $db->prepare( $query );
-  $rows = $sth2->execute;
-  if( $rows == 0 ) {
-    $query = "INSERT INTO wma_tile (x,y,z,rev) VALUES ('$x','$y','$zoom','$rev');";
-    #$sth2 = $db->prepare( $query );
-    #$rows = $sth2->execute;
-    #$tileid = $db->{ q{mysql_insertid} };
-  } else {
-    die "ambiguous tile" if( $rows > 1 );
-    ( $tileid ) = $sth2->fetchrow;
+  print "Zoom $zoom\n";
+
+  $query = "DELETE l.* FROM wma_label l, wma_tile t WHERE l.lang_id='$langid' AND l.rev='$rev' AND t.rev='$rev' AND t.z='$zoom';";
+  $sth = $db->prepare( $query );
+  $rows = $sth->execute;
+  print "Delete $rows labels in zoom $zoom from previous run.\n" if( $rows > 0 );
+
+  $start = time();
+  $query = "SELECT /* SLOW_OK */  FLOOR(x/2) as x2, FLOOR(y/2) as y2, t.id, l.weight, page_id FROM wma_label l, wma_tile t WHERE t.id = l.tile_id AND t.z='".($zoom+1)."' AND l.rev='$rev' AND t.rev='$rev' AND lang_id='$langid' ORDER BY x2,y2,l.weight;"; 
+  $sth = $db->prepare( $query );
+  $sth->execute;
+
+  $lasttid = -1;
+  $nbubble = 0; $ntotal = 0;
+  while( @row = $sth->fetchrow() ) 
+  {
+    $ntotal++;
+    ( $x, $y, $tid, $weight, $pageid ) = @row[0..4];
+    next if( $tid == $lasttid );
+    $lasttid = $tid;
+    $nbubble++;
+
+    # did we already insert a tile?
+    $query = "SELECT id FROM wma_tile WHERE z='$zoom' AND x='$x' AND y='$y' AND rev='$rev';";
+    $sth2 = $db->prepare( $query );
+    $rows = $sth2->execute;
+    if( $rows == 0 ) {
+      $query = "INSERT INTO wma_tile (x,y,z,rev) VALUES ('$x','$y','$zoom','$rev');";
+      $sth2 = $db->prepare( $query );
+      $rows = $sth2->execute;
+      $tileid = $db->{ q{mysql_insertid} };
+    } else {
+      die "ambiguous tile" if( $rows > 1 );
+      ( $tileid ) = $sth2->fetchrow;
+    }
+
+    # insert at zoom
+    $query = "INSERT INTO wma_label (tile_id,page_id,weight,lang_id,rev) VALUES ('$tileid','$pageid','$weight','$langid','$rev');";
+    $sth2 = $db->prepare( $query );
+    $rows = $sth2->execute;
   }
-
-  # insert at zoom
-  $query = "INSERT INTO wma_label (tile_id,page_id,weight,lang_id,rev) VALUES ('$tileid','$pageid','$weight','$langid','$rev');";
-  #$sth2 = $db->prepare( $query );
-  #$rows = $sth2->execute;
+ 
+  print " bubbled $nbubble out of $ntotal\n";
 }
-
