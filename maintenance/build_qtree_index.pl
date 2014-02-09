295ef63e17126de498f7d1430d1d62fb7a1b3aac
Daniel Schwen <daniel@schwen.de>
2014-02-09 04:23:49 +0000

    more adjustments for labs

--- maintenance/build_qtree_index.pl
+++ maintenance/build_qtree_index.pl
@@ -46,26 +46,27 @@ print "langid=$langid\n";
 die "unsupported language" if( $langid < 0 );
 
 my $database = $lang . "wiki_p";
-
-my $host = $lang . "wiki-p.userdb.toolserver.org";
-$host = "dewiki-p.userdb.toolserver.org" if( $lang eq "commons" );
+my $host = $lang . "wiki.labsdb";
 
 my $db = DBI->connect(
-  "DBI:mysql:database=$database;host=$host;mysql_use_result=0;mysql_read_default_file=" . getpwuid($<)->dir . "/.my.cnf",
+  "DBI:mysql:database=$database;host=$host;mysql_use_result=0;mysql_read_default_file=" . getpwuid($<)->dir . "/replica.my.cnf",
   undef, undef) or die "Error: $DBI::err, $DBI::errstr";
 
 print "Connected.\n";
   
-#create temporary blacklist table in u_dschwen
+my $wmadatabase = "p50380g50921__wma_p";
+my $gheldatabase = "p50380g50921__ghel_p";
+
+#create temporary blacklist table in $wmadatabase
 if( $langid != 4 ) {
-  $query = "CREATE TEMPORARY TABLE u_dschwen.blacklist (gc_from INT, cpp INT, cdn INT, dnf FLOAT);";
+  $query = "CREATE TEMPORARY TABLE $wmadatabase.blacklist (gc_from INT, cpp INT, cdn INT, dnf FLOAT);";
   $sth = $db->prepare( $query );
   $rows = $sth->execute;
-  $query = "INSERT /* SLOW_OK */ INTO u_dschwen.blacklist SELECT gc_from, count(*) AS cpp, COUNT(DISTINCT gc_name) AS cdn, COUNT(DISTINCT gc_name)/count(*) AS dnf FROM u_dispenser_p.coord_${lang}wiki c GROUP BY gc_from HAVING dnf<0.9 AND cpp>4;";
+  $query = "INSERT /* SLOW_OK */ INTO $wmadatabase.blacklist SELECT gc_from, count(*) AS cpp, COUNT(DISTINCT gc_name) AS cdn, COUNT(DISTINCT gc_name)/count(*) AS dnf FROM $gheldatabase.coord_${lang}wiki c GROUP BY gc_from HAVING dnf<0.9 AND cpp>4;";
   $sth = $db->prepare( $query ) or die;
   $rows = $sth->execute or die;
   print "Found $rows blacklisted articles.\n";
-  $query = "CREATE INDEX /* SLOW_OK */ from_index ON u_dschwen.blacklist (gc_from);";
+  $query = "CREATE INDEX /* SLOW_OK */ from_index ON $wmadatabase.blacklist (gc_from);";
   $sth = $db->prepare( $query ) or die;
   $rows = $sth->execute or die;
   print "Index created.\n";
@@ -76,16 +77,16 @@ $maxzoom = 14;
 
 undef @insert;
 
-#    $query = "create temporary table u_dschwen.compics ( page_id int, page_title varchar(255), el_to blob, img_width int, img_height int )";
+#    $query = "create temporary table $wmadatabase.compics ( page_id int, page_title varchar(255), el_to blob, img_width int, img_height int )";
 #    110     $sth = $db->prepare( $query );
 #    111     $sth->execute;
 #    112 
 #    113     print STDERR "Parparing: fetching all images with coordinates.\n";
-#    114     $query = "insert /* SLOW_OK */ into u_dschwen.compics SELECT page_id, page_title, el_to, img_width, img_height from image, page, externallinks where im    g_name = page_title and page_namespace=6 and el_to like '".$geohackurl."%' and el_from = page_id";
+#    114     $query = "insert /* SLOW_OK */ into $wmadatabase.compics SELECT page_id, page_title, el_to, img_width, img_height from image, page, externallinks where im    g_name = page_title and page_namespace=6 and el_to like '".$geohackurl."%' and el_from = page_id";
 #    115     $sth = $db->prepare( $query );
 #    116     $sth->execute;
 #    117 
-#    118     $query = "select /* SLOW_OK */ page_title, el_to, 0, GROUP_CONCAT( DISTINCT cl_to SEPARATOR '|'), img_width, img_height from u_dschwen.compics left joi    n categorylinks on page_id = cl_from group by page_id";
+#    118     $query = "select /* SLOW_OK */ page_title, el_to, 0, GROUP_CONCAT( DISTINCT cl_to SEPARATOR '|'), img_width, img_height from $wmadatabase.compics left joi    n categorylinks on page_id = cl_from group by page_id";
 #    119 }
 #    12
 
@@ -93,16 +94,16 @@ undef @insert;
 $start = time();
 $fac = ((1<<$maxzoom)*3)/180.0;
 if( $langid == 4 ) {
-  $query = "CREATE TEMPORARY TABLE u_dschwen.compics ( pid INT, lat DOUBLE(11,8), lon DOUBLE(11,8), title VARCHAR(255), head FLOAT, globe ENUM('','Mercury','Ariel','Phobos','Deimos','Mars','Rhea','Oberon','Europa','Tethys','Pluto','Miranda','Titania','Phoebe','Enceladus','Venus','Moon','Hyperion','Triton','Ceres','Dione','Titan','Ganymede','Umbriel','Callisto','Jupiter','Io','Earth','Mimas','Iapetus') )";
+  $query = "CREATE TEMPORARY TABLE $wmadatabase.compics ( pid INT, lat DOUBLE(11,8), lon DOUBLE(11,8), title VARCHAR(255), head FLOAT, globe ENUM('','Mercury','Ariel','Phobos','Deimos','Mars','Rhea','Oberon','Europa','Tethys','Pluto','Miranda','Titania','Phoebe','Enceladus','Venus','Moon','Hyperion','Triton','Ceres','Dione','Titan','Ganymede','Umbriel','Callisto','Jupiter','Io','Earth','Mimas','Iapetus') )";
   $sth = $db->prepare( $query );
   $sth->execute;
 
   print STDERR "Parparing: fetching all images with camera coordinates.\n";
 $query = <<QEND
-  INSERT INTO  u_dschwen.compics SELECT /* SLOW_OK */ 
+  INSERT INTO  $wmadatabase.compics SELECT /* SLOW_OK */ 
     page_id, gc_lat, gc_lon, page_title, gc_head, 
     CASE gc_globe WHEN '' THEN 'Earth' ELSE gc_globe END
-  FROM u_dispenser_p.coord_${lang}wiki c, page 
+  FROM $gheldatabase.coord_${lang}wiki c, page 
     WHERE page_namespace=6 
       AND c.gc_from=page_id
       AND gc_lat<=90.0 AND gc_lat>=-90.0
@@ -114,11 +115,11 @@ QEND
   $rows = $sth->execute or die "Error filling prep table: ".$db->errstr;
   print "PrepQuery completed in in ", ( time() - $start ), " seconds. $rows rows.\n";
 
-  $query = "CREATE INDEX pageid ON u_dschwen.compics (pid)";
+  $query = "CREATE INDEX pageid ON $wmadatabase.compics (pid)";
   $sth = $db->prepare( $query );
   $sth->execute or die "Error building index: ".$db->errstr;
 
-  $query = "CREATE INDEX pagetit ON u_dschwen.compics (title(10))";
+  $query = "CREATE INDEX pagetit ON $wmadatabase.compics (title(10))";
   $sth = $db->prepare( $query );
   $sth->execute or die "Error building index: ".$db->errstr;
 
@@ -126,9 +127,9 @@ $query = <<QEND
   SELECT /* SLOW_OK */ 
     pid, lat, lon, img_width, img_height, head, title, t.id, globe,
     GROUP_CONCAT( DISTINCT cl_to SEPARATOR '|')
-  FROM  image, u_dschwen.compics 
+  FROM  image, $wmadatabase.compics 
     LEFT JOIN categorylinks ON pid = cl_from 
-    LEFT JOIN u_dschwen.wma_tile t ON t.z=$maxzoom 
+    LEFT JOIN $wmadatabase.wma_tile t ON t.z=$maxzoom 
       AND t.x=FLOOR( (lon-FLOOR(lon/360)*360) * $fac ) AND t.y=FLOOR( (lat+90.0) * $fac )
     WHERE title = img_name
     GROUP BY pid
@@ -140,9 +141,9 @@ $query = <<QEND
     page_id, gc_lat, gc_lon, page_len, gc_size, gc_type,
     CASE WHEN gc_primary=1 THEN page_title ELSE gc_name END, t.id, gc_primary,
     CASE gc_globe WHEN '' THEN 'Earth' ELSE gc_globe END
-  FROM page, u_dispenser_p.coord_${lang}wiki c 
-    LEFT JOIN u_dschwen.blacklist b ON b.gc_from=c.gc_from
-    LEFt JOIN u_dschwen.wma_tile t ON t.z=$maxzoom 
+  FROM page, $gheldatabase.coord_${lang}wiki c 
+    LEFT JOIN $wmadatabase.blacklist b ON b.gc_from=c.gc_from
+    LEFt JOIN $wmadatabase.wma_tile t ON t.z=$maxzoom 
       AND t.x=FLOOR( (gc_lon-FLOOR(gc_lon/360)*360) * $fac ) AND t.y=FLOOR( (gc_lat+90.0) * $fac )
     WHERE page_namespace=0 AND c.gc_from=page_id  
       AND gc_lat<=90.0 AND gc_lat>=-90.0
@@ -158,9 +159,8 @@ $rows = $sth->execute or die;
 print STDERR  $db->errstr;
 print "Query completed in in ", ( time() - $start ), " seconds. $rows rows.\n";
 
-my $userdatabase = "u_dschwen";
 my $db2 = DBI->connect(
-  "DBI:mysql:database=$userdatabase;host=$host;mysql_multi_statements=1;mysql_read_default_file=" . getpwuid($<)->dir . "/.my.cnf",
+  "DBI:mysql:database=$wmadatabase;host=$host;mysql_multi_statements=1;mysql_read_default_file=" . getpwuid($<)->dir . "/.my.cnf",
   undef, undef) or die "Error: $DBI::err, $DBI::errstr";
 
 $query = "DELETE /* SLOW_OK */ c.*, l.* FROM wma_connect c, wma_label l WHERE c.label_id = l.id AND c.rev='$rev' AND l.lang_id='$langid';";
@@ -244,7 +244,7 @@ while( @row = $sth->fetchrow() )
 
     # make sure the connection is there
     #$db2 ||= DBI->connect(
-    #  "DBI:mysql:database=$userdatabase;host=$host;mysql_multi_statements=1;mysql_read_default_file=" . getpwuid($<)->dir . "/.my.cnf",
+    #  "DBI:mysql:database=$wmadatabase;host=$host;mysql_multi_statements=1;mysql_read_default_file=" . getpwuid($<)->dir . "/.my.cnf",
     #  undef, undef) or die "Error: $DBI::err, $DBI::errstr";
 
     $sth2 = $db2->prepare( $query ) or die;
@@ -260,7 +260,7 @@ while( @row = $sth->fetchrow() )
     # make sure the connection is there
     #while(1) {
     #  $db2 ||= DBI->connect(
-    #    "DBI:mysql:database=$userdatabase;host=$host;mysql_multi_statements=1;mysql_read_default_file=" . getpwuid($<)->dir . "/.my.cnf",
+    #    "DBI:mysql:database=$wmadatabase;host=$host;mysql_multi_statements=1;mysql_read_default_file=" . getpwuid($<)->dir . "/.my.cnf",
     #    undef, undef) or die "Error: $DBI::err, $DBI::errstr";
 
       $sth2 = $db2->prepare( $query ) or next;
@@ -277,7 +277,7 @@ if( scalar(@insert) > 0 ) {
   # make sure the connection is there
   #while(1) {
   #  $db2 ||= DBI->connect(
-  #    "DBI:mysql:database=$userdatabase;host=$host;mysql_multi_statements=1;mysql_read_default_file=" . getpwuid($<)->dir . "/.my.cnf",
+  #    "DBI:mysql:database=$wmadatabase;host=$host;mysql_multi_statements=1;mysql_read_default_file=" . getpwuid($<)->dir . "/.my.cnf",
   #    undef, undef) or die "Error: $DBI::err, $DBI::errstr";
 
     $sth2 = $db2->prepare( $query ) or next;
--- maintenance/wma_initialize_tables.sh
+++ maintenance/wma_initialize_tables.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-#  `seq 1 7`
-for c in 2
-do
-  server=hap-s$c-user.esi.toolserver.org
-  echo server $server
-  mysql -h${server} -e "CREATE DATABASE IF NOT EXISTS u_dschwen;"
-  mysql -h${server} u_dschwen < wma_schema.sql
-  #mysql -h${server} u_dschwen < wma2_schema.sql
-  #mysql -h${server} u_dschwen < wma2_storedprocedures.sql
-  mysql -h${server} u_dschwen < wma_storedprocedures.sql
-  #mysql -hsql-s${c}-user.toolserver.org u_dschwen -e "ALTER TABLE wma_label ADD COLUMN globe enum('','Mercury','Ariel','Phobos','Deimos','Mars','Rhea','Oberon','Europa','Tethys','Pluto','Miranda','Titania','Phoebe','Enceladus','Venus','Moon','Hyperion','Triton','Ceres','Dione','Titan','Ganymede','Umbriel','Callisto','Jupiter','Io','Earth','Mimas','Iapetus');"
- # mysql -hsql-s${c}-user.toolserver.org u_dschwen -e "ALTER TABLE wma_label CHANGE lat lat DOUBLE(11,8);"
- # mysql -hsql-s${c}-user.toolserver.org u_dschwen -e "ALTER TABLE wma_label CHANGE lon lon DOUBLE(11,8);"
-  #mysql -hsql-s${c}-user.toolserver.org u_dschwen -e "UPDATE wma_label SET globe='Earth';";
-done
+server=localhost
+echo server $server
+mysql -h${server} -e "CREATE DATABASE IF NOT EXISTS wma;"
+mysql -h${server} wma < wma_schema.sql
+mysql -h${server} wma < wma_storedprocedures.sql
+
+
+#mysql -h${server} u_dschwen < wma2_schema.sql
+#mysql -h${server} u_dschwen < wma2_storedprocedures.sql
+#mysql -hsql-s${c}-user.toolserver.org u_dschwen -e "ALTER TABLE wma_label ADD COLUMN globe enum('','Mercury','Ariel','Phobos','Deimos','Mars','Rhea','Oberon','Europa','Tethys','Pluto','Miranda','Titania','Phoebe','Enceladus','Venus','Moon','Hyperion','Triton','Ceres','Dione','Titan','Ganymede','Umbriel','Callisto','Jupiter','Io','Earth','Mimas','Iapetus');"
+# mysql -hsql-s${c}-user.toolserver.org u_dschwen -e "ALTER TABLE wma_label CHANGE lat lat DOUBLE(11,8);"
+# mysql -hsql-s${c}-user.toolserver.org u_dschwen -e "ALTER TABLE wma_label CHANGE lon lon DOUBLE(11,8);"
+#mysql -hsql-s${c}-user.toolserver.org u_dschwen -e "UPDATE wma_label SET globe='Earth';";
 
 #mysql -hcommonswiki-p.userdb.toolserver.org u_dschwen < wma_tile.schema.sql
 #mysql -hcommonswiki-p.userdb.toolserver.org u_dschwen < wma_connect.schema.sql
