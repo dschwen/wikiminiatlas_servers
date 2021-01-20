df95e90b77c13c0c44349be1db6351fc319e37d2
Daniel Schwen <daniel@schwen.de>
2021-01-19 21:53:24 -0700

    php include

---
+++ maintenance/update_php_include.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+#
+# WikiMiniAtlas database update script (c) 2009-2021 Daniel Schwen
+# Licensed under the GPL3 license
+#
+
+import pickle
+import sys
+import os
+
+#
+# globes
+#
+with open(os.path.join(os.path.dirname(__file__), 'globes.dat')) as globes_file:
+    globes_list = [i.rstrip('\n') for i in globes_file.readlines()]
+
+#
+# languages
+#
+with open(os.path.join(os.path.dirname(__file__), 'languages.dat')) as lang_file:
+    lang_list = [i.rstrip('\n') for i in lang_file.readlines()]
+lang = {lang_list[i].lower(): i for i in range(len(lang_list))}
+
+#
+# rev id cache
+#
+try:
+    rev_file = open(".cache/rev.pickle", "rb")
+    rev_dict = pickle.load(rev_file)
+    rev_file.close()
+except:
+    rev_dict = {lang: 1 for lang in lang_list}
+
+#
+# write master PHP include
+#
+with open(os.path.join(os.path.dirname(__file__), 'master.inc'), "w") as inc_file:
+    inc_file.write("<?php\n")
+
+    # globes
+    inc_file.write("$globes = array(")
+    inc_file.write(", ".join(["'%s'=>%d" % (globes_list[i].lower(), i) for i in range(len(globes_list))]))
+    inc_file.write(");\n")
+
+    # languages
+    inc_file.write("$langs = array(")
+    inc_file.write(", ".join(["'%s'=>%d" % (lang_list[i].lower(), i) for i in range(len(lang_list))]))
+    inc_file.write(");\n")
+
+    # revisions
+    inc_file.write("$rev = array(")
+    inc_file.write(", ".join(["'%s'=>%d" % (lang, rev) for lang, rev in rev_dict.items()]))
+    inc_file.write(");\n")
+
+    inc_file.write("?>\n")
