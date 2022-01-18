644d1f2db109011e65f0f4e51795e33ec8990d3f
Daniel Schwen <daniel@schwen.de>
2022-01-18 23:40:46 +0000

    add systemd scripts

---
+++ scripts/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for LEVEL in 8 9 10 11 12
+do
+  svc=wma-mapnik-${LEVEL}.service
+  export LEVEL
+  envsubst < wma-mapnik.service > $svc
+  sudo cp $svc /lib/systemd/system
+  pushd /etc/systemd/system
+  sudo ln -sf /lib/systemd/system/$svc .
+  popd
+  sudo systemctl daemon-reload
+  sudo systemctl start $svc
+
+  echo check with: sudo journalctl -e -u $svc
+done
+
