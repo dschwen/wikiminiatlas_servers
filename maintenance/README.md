b34c0d681def840c856b4c25c936b3018a621ec6
Daniel Schwen <daniel@schwen.de>
2022-01-25 22:18:57 +0000

    fix tile cutter, update readme

--- maintenance/README.md
+++ maintenance/README.md
@@ -1,4 +1,16 @@
-# Hook up label database meta data
+# Coordinate ectration scripts
+
+## Prerequisite
+
+Install teh `toolforge` python module
+
+```
+git clone https://github.com/legoktm/toolforge.git
+cd toolforge
+python3 setup.py install --user
+```
+
+## Hook up label database meta data
 
 Create these two symlinks in the webserver root
 
--- scripts/imagery/generate_tiles.py
+++ scripts/imagery/generate_tiles.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 
-import pyvips
 import sys
 
+if len(sys.argv) != 3 :
+    print("usage:", sys.argv[0], 'input_image output_prefix')
+    sys.exit(1)
+
+import pyvips
+import math
+
+# tile size
+ts = 256
+
+print("Opening...")
 img = pyvips.Image.new_from_file(sys.argv[1])
-w = img.width
-h = img.height
-max_z = int(math.ceil(math.log2(img.height/(3*256))))
+iw = img.width
+ih = img.height
+max_z = int(math.ceil(math.log2(ih/(3*ts))))
+
+print("Rescaling...")
+h = (1 << max_z) * 3 * ts
+w = 2 * h
+img = img.thumbnail_image(w, height=h)
+
+print("Tiling...")
+img.dzsave(sys.argv[2], tile_size=ts, overlap=0, depth='onetile', layout='google')
